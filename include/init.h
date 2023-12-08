4cfc87ceaef26a361c42d95af8d97d27a21ea099
hbinhng <binhmattroi@gmail.com>
2023-12-08 11:54:49 +0700

    Finalize init

--- include/init.h
+++ include/init.h
@@ -1,6 +1,6 @@
 #ifndef RTES_FINAL_INCLUDE_INIT_H
 #define RTES_FINAL_INCLUDE_INIT_H
 
-inline void init();
+void init();
 
 #endif // RTES_FINAL_INCLUDE_INIT_H
--- src/init.c
+++ src/init.c
@@ -1,6 +1,11 @@
 #include "fsl_device_registers.h"
+#include "fsl_debug_console.h"
+#include "board.h"
+#include "MKL46Z4.h"
+#include "pin_mux.h"
 
 #include "../include/init.h"
+#include "../lib/Seg_LCD.h"
 
 #define GPIOMODE 0x100
 #define BPORT_CLOCK 0x400
@@ -13,7 +18,6 @@
 
 #define GLED_VREG (PTD->PDOR)
 
-static inline void alertsInit();
 void alertsInit() {	
 	/* Enable LEDs */
 	
@@ -21,30 +25,27 @@ void alertsInit() {
 	PORTD->PCR[GLEDPIN] = GPIOMODE;
 	PTD->PDDR |= GLEDBIT;
 	GLED_VREG |= GLEDBIT;
+	GLED_VREG ^= GLEDBIT;
 	
 	/* Enable LCD */
 	
-	// Initialize ports
-	/* LCD_01 */ PORTD->PCR[0]  = GPIOMODE;
-	/* LCD_02 */ PORTE->PCR[4]  = GPIOMODE;
-	/* LCD_03 */ PORTB->PCR[23] = GPIOMODE;
-	/* LCD_04 */ PORTB->PCR[22] = GPIOMODE;
-	/* LCD_05 */ PORTC->PCR[17] = GPIOMODE;
-	/* LCD_06 */ PORTB->PCR[21] = GPIOMODE;
-	/* LCD_07 */ PORTB->PCR[7]  = GPIOMODE;
-	/* LCD_08 */ PORTB->PCR[8]  = GPIOMODE;
-	/* LCD_09 */ PORTE->PCR[5]  = GPIOMODE;
-	/* LCD_10 */ PORTC->PCR[18] = GPIOMODE;
-	/* LCD_11 */ PORTB->PCR[10] = GPIOMODE;
-	/* LCD_12 */ PORTB->PCR[11] = GPIOMODE;
+	SegLCD_Init();
 }
 
-static inline void actionsInit();
 void actionsInit() {
+	/* Enable Switches */
+	
+	PORTC->PCR[3] = GPIOMODE | PORT_PCR_PE_MASK | PORT_PCR_PS_MASK;
+	PTC->PDDR &= ~0x8;
 }
 
 void init() {
-	SIM->SCGC5 |= BPORT_CLOCK | CPORT_CLOCK | DPORT_CLOCK | EPORT_CLOCK;
+	SIM->SCGC5 = SIM->SCGC5
+		| SIM_SCGC5_PORTB_MASK
+		| SIM_SCGC5_PORTC_MASK
+		| SIM_SCGC5_PORTD_MASK
+		| SIM_SCGC5_PORTE_MASK
+		| SIM_SCGC5_SLCD_MASK;
 	
 	alertsInit();
 	actionsInit();
