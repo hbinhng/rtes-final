edb59485f4897a1068cbcb9fbc374ebd6a2d66e4
hbinhng <binhmattroi@gmail.com>
2023-12-14 23:14:23 +0700

    Implement blinker

--- src/alerts.c
+++ src/alerts.c
@@ -1,18 +1,29 @@
+#include "fsl_device_registers.h"
+#include "fsl_debug_console.h"
+#include "board.h"
+#include "MKL46Z4.h"
+#include "pin_mux.h"
+
 #include "../include/alerts.h"
 #include "../lib/Seg_LCD.h"
 
 _Bool blinking = 0;
 
-#define MAX_TIMER_START (2147483647)
 #define TICK_INTERVAL 5
 #define last(value) ((value) % 10)
 
-int alertTimerStart = MAX_TIMER_START;
+int alertTimer = 0;
 
 void SysTick_Alert_Handler(void) {
-	if (alertTimerStart == MAX_TIMER_START) return;
+	if (alertTimer > 1e9)
+		alertTimer = 0;
 	
-	alertTimerStart += TICK_INTERVAL;
+	alertTimer += TICK_INTERVAL;
+	
+	if (blinking == 1) {
+		if (alertTimer % 50 == 0)
+			LED_GREEN_TOGGLE();
+	}
 }
 
 void setDisplay(char value) {
@@ -22,8 +33,11 @@ void setDisplay(char value) {
 }
 
 void startBlink(void) {
-	
+	blinking = 1;
 }
 
 void stopBlink(void) {
+	blinking = 0;
+	
+	LED_GREEN_OFF();
 }
