06cfdd361bf047dc5cb53dedfc205be9bfc710e0
hbinhng <binhmattroi@gmail.com>
2023-12-15 11:46:57 +0700

    Add beltTrigger, switch macros

--- include/actions.h
+++ include/actions.h
@@ -3,7 +3,13 @@
 
 #include "state.h"
 
+#define SW1_PIN (3)
+#define SW1_MASK (1<<SW1_PIN)
+#define SW3_PIN (12)
+#define SW3_MASK (1<<SW3_PIN)
+
 void seatTrigger(State *state, _Bool value);
+void beltTrigger(State *state, _Bool value);
 
 void getInCar(State *state);
 void getOutCar(State *state);
